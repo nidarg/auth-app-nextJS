6c75e1206568b9ced5f6da6e652116e127abf4e0
Gradin Gheti <gradin_gheti@netex.ro>
2024-05-29 16:27:41 +0300

    first commit

---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { Card } from "../ui/card"\
+
+interface CardWrapperProps{
+ children:React.ReactNode;
+ headerLabel: string;
+ backButtonLabel: string;
+ backButtonHref: string;
+ showSocial?: boolean;
+}
+
+export const CardWrapper = ({
+ children,headerLabel,backButtonLabel,backButtonHref,showSocial
+}:CardWrapperProps)=>{
+ return(
+
+  
+ )
+}
