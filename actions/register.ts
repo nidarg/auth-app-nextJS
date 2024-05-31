a608992ebb1620e95071cf83e4e51ca8cc711332
Gradin Gheti <gradin_gheti@netex.ro>
2024-05-31 16:29:51 +0300

    set next auth

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,10 @@
 'use server';
 
+import bcrypt from 'bcryptjs';
 import * as z from 'zod';
 import { RegisterSchema } from '@/schemas';
+import { db } from '@/lib/db';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -9,5 +12,26 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validatedFields.success) {
     return { error: 'Invalide fields' };
   }
-  return { success: 'Email sent!' };
+
+  const { name, email, password } = validatedFields.data;
+  // hash passwd
+  const hashedPassword = await bcrypt.hash(password, 10);
+  // verify unique email
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: 'Email already in use' };
+  }
+  //  save user to db
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO send verification token email
+
+  return { success: 'Account created' };
 };
